2ffe9ed2babef1a134d01f060b200341e6261ab7
Abb1x <abb1x@protonmail.com>
2021-02-27 16:26:17 -0500

    Meta: basic sdl example for pherelo

--- src/main.d
+++ src/main.d
@@ -7,6 +7,44 @@ import bindbc.sdl.image;
 
 void main()
 {
+    loadSDL();
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        writeln("SDL_Init: ", fromStringz(SDL_GetError()));
+    }
+    else
+        SDL_Quit();
 
-write("hello");
+    SDL_Window *window = SDL_CreateWindow(
+        "Example Window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        800,
+        600,
+        SDL_WINDOW_OPENGL);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+
+            if (event.type == SDL_KEYDOWN)
+            {
+                quit = true;
+            }
+        }
+    }
 }
