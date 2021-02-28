324068ce2efb6aae5906dae6e27bcd9aeb9979f0
Abb1x <abb1x@protonmail.com>
2021-02-28 09:56:48 -0500

    GUI: performance improvement
    From 30%+ cpu usage to 4%

--- lib/gui.c
+++ lib/gui.c
@@ -4,6 +4,41 @@ SDL_Window *window;
 SDL_Renderer *renderer;
 bool is_running = false;
 
+static float get_time_in_seconds()
+{
+    float t = SDL_GetTicks();
+    t *= 0.001f;
+    return t;
+}
+
+void GUI_handle_events()
+{
+    SDL_Event event;
+    if (SDL_WaitEvent(&event) != 0)
+    {
+        switch (event.type)
+        {
+
+        case SDL_QUIT:
+            is_running = false;
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+int GUI_get_refresh_rate()
+{
+    int displayIndex = SDL_GetWindowDisplayIndex(window);
+    SDL_DisplayMode mode;
+
+    SDL_GetDisplayMode(displayIndex, 0, &mode);
+
+    return mode.refresh_rate;
+}
+
 void GUI_init(Colorscheme colorscheme)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
@@ -17,21 +52,41 @@ void GUI_init(Colorscheme colorscheme)
 
     SDL_SetRenderDrawColor(renderer, colorscheme.bg.r, colorscheme.bg.g, colorscheme.bg.b, colorscheme.bg.a);
 
+    /* Limiting the game loop */
+    const float timestep = 0.01f;
+    float accumulator = 0.0f;
+
+    float current_time = get_time_in_seconds();
     while (is_running)
     {
+
+        int start_tick = SDL_GetTicks();
+        float new_time = get_time_in_seconds();
+        float frame_time = new_time - current_time;
+
+        accumulator += frame_time;
+
+        /* Handling events */
+        while (accumulator >= timestep)
+
+        {
+            GUI_handle_events();
+            accumulator -= timestep;
+        }
+
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
-        SDL_Event event;
-        SDL_PollEvent(&event);
-        switch (event.type)
+
+        int frame_ticks = SDL_GetTicks() - start_tick;
+
+        if (frame_ticks < 1000 / GUI_get_refresh_rate())
         {
-        case SDL_QUIT:
-            is_running = false;
-            break;
-        default:
-            break;
+            SDL_Delay(1000 / GUI_get_refresh_rate() - frame_ticks);
         }
     }
-    free(window);
-    free(renderer);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 }
